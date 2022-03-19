96b2e2133f5525aeaa463590d0aafc0b6ccbb356
Pulat <pulatnazarov8899@gmail.com>
2022-03-19 09:45:40 +0500

    save-1 for my program

---
+++ shunchaki.py
@@ -0,0 +1,13 @@
+a='meni aytyapti'
+for i in a:
+    if i=='a':
+        continue
+    print(i*3,end='')
+print()
+b='menda uuu'
+for i in b:
+    if i=='a':
+        break
+    print(i*2,end='')
+
+#a="Ozbekiston"
